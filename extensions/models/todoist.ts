84e6ff9807e2d6bc9ac0c7123b820aa7ba9716b4
Mat Greten <mat.greten@companycam.com>
2026-06-27 16:05:21 -0600

    Quality fixes, adversarial review, bump version to 2026.06.27.1

--- extensions/models/todoist.ts
+++ extensions/models/todoist.ts
@@ -80,7 +80,7 @@ function getClient(
  */
 export const model = {
   type: "@mgreten/todoist",
-  version: "2026.06.08.2",
+  version: "2026.06.27.1",
   globalArguments: GlobalArgsSchema,
   resources: {
     task: {
